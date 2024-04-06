87c709bef9c9e987fe1e306f0f4d0d55ed03ac46
Rodrigo Pelorosso <rodrigo.pelorosso@gmail.com>
2024-04-05 21:41:54 -0300

    Add chmod

---
+++ src/3-chmod/chmod.asm
@@ -0,0 +1,13 @@
+;
+;   Utiliza la función del bios CHMOD para iniciar el modo 1 de pantalla
+;   No hace falta hacer un loop para que no salga el programa, porque modo 1 soporta texto
+;
+
+.ZILOG
+.BASIC
+.ORG 0xC000
+
+CHGMOD equ 0x005F
+
+ld a, 1      ; a = 1
+call CHGMOD  ; llama a funcion del bios que cambia el modo gráfico que indica el registro A
